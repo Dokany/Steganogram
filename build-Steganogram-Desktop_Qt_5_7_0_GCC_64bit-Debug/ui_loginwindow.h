acdf54b6066481f353939b80c2dba03845da4551
John Sourour <johnsourour@aucegypt.edu>
2017-11-26 11:34:28 +0200

    Add files via upload

---
+++ build-Steganogram-Desktop_Qt_5_7_0_GCC_64bit-Debug/ui_loginwindow.h
@@ -0,0 +1,467 @@
+/********************************************************************************
+** Form generated from reading UI file 'loginwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_LOGINWINDOW_H
+#define UI_LOGINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_LoginWindow
+{
+public:
+    QWidget *centralWidget;
+    QGroupBox *groupBox;
+    QLineEdit *UsernameLineEdit;
+    QLineEdit *PasswordLineEdit;
+    QPushButton *LoginButton;
+    QPushButton *SignUpButton;
+    QLabel *LogoLabel;
+    QLabel *authError;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *LoginWindow)
+    {
+        if (LoginWindow->objectName().isEmpty())
+            LoginWindow->setObjectName(QStringLiteral("LoginWindow"));
+        LoginWindow->resize(330, 355);
+        LoginWindow->setStyleSheet(QLatin1String("QMainWindow{\n"
+"background-color:#253147;\n"
+"}\n"
+"\n"
+"QMenuBar{\n"
+"background-color:#1d1d1d;\n"
+"padding:5px;\n"
+"	font: 12pt \"MS Shell Dlg 2\";\n"
+"}\n"
+"\n"
+"QMenuBar::item{\n"
+"background-color:#1d1d1d;\n"
+"color:#fff;\n"
+"padding:5px;\n"
+"\n"
+"}\n"
+"\n"
+"QMenu{\n"
+"color:#fff;\n"
+"padding:0;\n"
+"}\n"
+"\n"
+"QMenu::item:selected{\n"
+"color:#fff;\n"
+"background-color:#00aba9;\n"
+"}\n"
+"\n"
+"QGroupBox {\n"
+"    background-color: #253147;\n"
+"}\n"
+"\n"
+"QTableWidget{\n"
+"background-color:#3d3d3d;\n"
+"color:#fff;\n"
+"  selection-background-color: #da532c;\n"
+"border:solid;\n"
+"border-width:3px;\n"
+"border-color:#da532c;\n"
+"}\n"
+"QHeaderView::section{\n"
+"background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(20, 158, 217, 255), stop:1 rgba(36, 158, 217, 255));\n"
+"border:none;\n"
+"border-top-style:solid;\n"
+"border-width:1px;\n"
+"border-top-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(20, 158, 217, 255), stop:1 rgba(36, 158, 217, 255));\n"
+"color:#fff;\n"
+""
+                        "\n"
+"}\n"
+"QHeaderView{\n"
+"background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(20, 158, 217, 255), stop:1 rgba(36, 158, 217, 255));\n"
+"\n"
+"border:none;\n"
+"border-top-style:solid;\n"
+"border-width:1px;\n"
+"border-top-color:#149ED9;\n"
+"color:#fff;\n"
+"	font: 75 12pt \"Calibri\";\n"
+"}\n"
+"\n"
+"QTableCornerButton::section{\n"
+"border:none;\n"
+"background-color:#149ED9;\n"
+"}\n"
+"\n"
+"QListWidget{\n"
+"background-color:#6A6A6A;\n"
+"color:#fff;\n"
+"}\n"
+"\n"
+"QMenu{\n"
+"background-color:#253147;\n"
+"}\n"
+"QStatusBar{\n"
+"background-color:#253147;\n"
+"color:#fff;\n"
+"}\n"
+"\n"
+"QPushButton{\n"
+"border-style:solid;\n"
+"\n"
+"background-color:#e3a21a;\n"
+"color:#fff;\n"
+"border-radius:7px;\n"
+"}\n"
+"QPushButton:hover{\n"
+"color:#ccc;\n"
+"	background-color: qlineargradient(spread:pad, x1:0.517, y1:0, x2:0.517, y2:1, stop:0 rgba(45, 45, 45, 255), stop:0.505682 rgba(45, 45, 45, 255), stop:1 rgba(29, 29, 29, 255));\n"
+"	border-color:#2d89ef;\n"
+"border-width:2px;\n"
+"}\n"
+"\n"
+"QPushButton:"
+                        "pressed{\n"
+"background-color: qlineargradient(spread:pad, x1:0.517, y1:0, x2:0.517, y2:1, stop:0 rgba(29, 29, 29, 255), stop:0.505682 rgba(45, 45, 45, 255), stop:1 rgba(29, 29, 29, 255));\n"
+"}\n"
+"\n"
+"\n"
+"QTabWidget::tab{\n"
+"background-color:#253147;\n"
+"}\n"
+"\n"
+"QLineEdit{\n"
+"border-radius:0;\n"
+"}\n"
+"\n"
+"QProgressBar{\n"
+"border-radius:0;\n"
+"text-align:center;\n"
+"color:#fff;\n"
+"background-color:transparent;\n"
+"border: 2px solid #e3a21a;\n"
+"border-radius:7px;\n"
+"	font: 75 12pt \"Open Sans\";\n"
+"\n"
+"}\n"
+"\n"
+"QProgressBar::chunk{\n"
+"background-color:#2d89ef;\n"
+"width:20px;\n"
+"}"));
+        centralWidget = new QWidget(LoginWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        groupBox = new QGroupBox(centralWidget);
+        groupBox->setObjectName(QStringLiteral("groupBox"));
+        groupBox->setGeometry(QRect(0, 0, 331, 331));
+        groupBox->setStyleSheet(QLatin1String("QMainWindow{\n"
+"background-color:#253147;\n"
+"}\n"
+"\n"
+"QMenuBar{\n"
+"background-color:#1d1d1d;\n"
+"padding:5px;\n"
+"	font: 12pt \"MS Shell Dlg 2\";\n"
+"}\n"
+"\n"
+"QMenuBar::item{\n"
+"background-color:#1d1d1d;\n"
+"color:#fff;\n"
+"padding:5px;\n"
+"\n"
+"}\n"
+"\n"
+"QMenu{\n"
+"color:#fff;\n"
+"padding:0;\n"
+"}\n"
+"\n"
+"QMenu::item:selected{\n"
+"color:#fff;\n"
+"background-color:#00aba9;\n"
+"}\n"
+"\n"
+"QGroupBox {\n"
+"    background-color: #253147;\n"
+"}\n"
+"\n"
+"QTableWidget{\n"
+"background-color:#3d3d3d;\n"
+"color:#fff;\n"
+"  selection-background-color: #da532c;\n"
+"border:solid;\n"
+"border-width:3px;\n"
+"border-color:#da532c;\n"
+"}\n"
+"QHeaderView::section{\n"
+"background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(20, 158, 217, 255), stop:1 rgba(36, 158, 217, 255));\n"
+"border:none;\n"
+"border-top-style:solid;\n"
+"border-width:1px;\n"
+"border-top-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(20, 158, 217, 255), stop:1 rgba(36, 158, 217, 255));\n"
+"color:#fff;\n"
+""
+                        "\n"
+"}\n"
+"QHeaderView{\n"
+"background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(20, 158, 217, 255), stop:1 rgba(36, 158, 217, 255));\n"
+"\n"
+"border:none;\n"
+"border-top-style:solid;\n"
+"border-width:1px;\n"
+"border-top-color:#149ED9;\n"
+"color:#fff;\n"
+"	font: 75 12pt \"Calibri\";\n"
+"}\n"
+"\n"
+"QTableCornerButton::section{\n"
+"border:none;\n"
+"background-color:#149ED9;\n"
+"}\n"
+"\n"
+"QListWidget{\n"
+"background-color:#6A6A6A;\n"
+"color:#fff;\n"
+"}\n"
+"\n"
+"QMenu{\n"
+"background-color:#253147;\n"
+"}\n"
+"QStatusBar{\n"
+"background-color:#253147;\n"
+"color:#fff;\n"
+"}\n"
+"\n"
+"QPushButton{\n"
+"border-style:solid;\n"
+"\n"
+"background-color:#e3a21a;\n"
+"color:#fff;\n"
+"border-radius:7px;\n"
+"}\n"
+"QPushButton:hover{\n"
+"color:#ccc;\n"
+"	background-color: qlineargradient(spread:pad, x1:0.517, y1:0, x2:0.517, y2:1, stop:0 rgba(45, 45, 45, 255), stop:0.505682 rgba(45, 45, 45, 255), stop:1 rgba(29, 29, 29, 255));\n"
+"	border-color:#2d89ef;\n"
+"border-width:2px;\n"
+"}\n"
+"\n"
+"QPushButton:"
+                        "pressed{\n"
+"background-color: qlineargradient(spread:pad, x1:0.517, y1:0, x2:0.517, y2:1, stop:0 rgba(29, 29, 29, 255), stop:0.505682 rgba(45, 45, 45, 255), stop:1 rgba(29, 29, 29, 255));\n"
+"}\n"
+"\n"
+"\n"
+"QTabWidget::tab{\n"
+"background-color:#253147;\n"
+"}\n"
+"\n"
+"QLineEdit{\n"
+"border-radius:0;\n"
+"}\n"
+"\n"
+"QProgressBar{\n"
+"border-radius:0;\n"
+"text-align:center;\n"
+"color:#fff;\n"
+"background-color:transparent;\n"
+"border: 2px solid #e3a21a;\n"
+"border-radius:7px;\n"
+"	font: 75 12pt \"Open Sans\";\n"
+"\n"
+"}\n"
+"\n"
+"QProgressBar::chunk{\n"
+"background-color:#2d89ef;\n"
+"width:20px;\n"
+"}"));
+        UsernameLineEdit = new QLineEdit(groupBox);
+        UsernameLineEdit->setObjectName(QStringLiteral("UsernameLineEdit"));
+        UsernameLineEdit->setGeometry(QRect(20, 130, 291, 31));
+        PasswordLineEdit = new QLineEdit(groupBox);
+        PasswordLineEdit->setObjectName(QStringLiteral("PasswordLineEdit"));
+        PasswordLineEdit->setGeometry(QRect(20, 170, 291, 31));
+        PasswordLineEdit->setEchoMode(QLineEdit::Password);
+        LoginButton = new QPushButton(groupBox);
+        LoginButton->setObjectName(QStringLiteral("LoginButton"));
+        LoginButton->setGeometry(QRect(10, 240, 311, 32));
+        SignUpButton = new QPushButton(groupBox);
+        SignUpButton->setObjectName(QStringLiteral("SignUpButton"));
+        SignUpButton->setGeometry(QRect(10, 280, 311, 32));
+        LogoLabel = new QLabel(groupBox);
+        LogoLabel->setObjectName(QStringLiteral("LogoLabel"));
+        LogoLabel->setGeometry(QRect(100, 20, 131, 111));
+        LogoLabel->setAlignment(Qt::AlignCenter);
+        authError = new QLabel(groupBox);
+        authError->setObjectName(QStringLiteral("authError"));
+        authError->setGeometry(QRect(40, 210, 251, 20));
+        LoginWindow->setCentralWidget(centralWidget);
+        mainToolBar = new QToolBar(LoginWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        mainToolBar->setStyleSheet(QLatin1String("QMainWindow{\n"
+"background-color:#6A6A6A;\n"
+"}\n"
+"\n"
+"QMenuBar{\n"
+"background-color:#1d1d1d;\n"
+"padding:5px;\n"
+"	font: 12pt \"MS Shell Dlg 2\";\n"
+"}\n"
+"\n"
+"QMenuBar::item{\n"
+"background-color:#1d1d1d;\n"
+"color:#fff;\n"
+"padding:5px;\n"
+"\n"
+"}\n"
+"\n"
+"QMenu{\n"
+"color:#fff;\n"
+"padding:0;\n"
+"}\n"
+"\n"
+"QMenu::item:selected{\n"
+"color:#fff;\n"
+"background-color:#00aba9;\n"
+"}\n"
+"\n"
+"QTableWidget{\n"
+"background-color:#3d3d3d;\n"
+"color:#fff;\n"
+"  selection-background-color: #da532c;\n"
+"border:solid;\n"
+"border-width:3px;\n"
+"border-color:#da532c;\n"
+"}\n"
+"QHeaderView::section{\n"
+"background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(20, 158, 217, 255), stop:1 rgba(36, 158, 217, 255));\n"
+"border:none;\n"
+"border-top-style:solid;\n"
+"border-width:1px;\n"
+"border-top-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(20, 158, 217, 255), stop:1 rgba(36, 158, 217, 255));\n"
+"color:#fff;\n"
+"\n"
+"}\n"
+"QHeaderView{\n"
+"background-color:qlineargradient"
+                        "(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(20, 158, 217, 255), stop:1 rgba(36, 158, 217, 255));\n"
+"\n"
+"border:none;\n"
+"border-top-style:solid;\n"
+"border-width:1px;\n"
+"border-top-color:#149ED9;\n"
+"color:#fff;\n"
+"	font: 75 12pt \"Calibri\";\n"
+"}\n"
+"\n"
+"QTableCornerButton::section{\n"
+"border:none;\n"
+"background-color:#149ED9;\n"
+"}\n"
+"\n"
+"QListWidget{\n"
+"background-color:#3d3d3d;\n"
+"color:#fff;\n"
+"}\n"
+"\n"
+"QMenu{\n"
+"background-color:#3d3d3d;\n"
+"}\n"
+"QStatusBar{\n"
+"background-color:#7e3878;\n"
+"color:#fff;\n"
+"}\n"
+"\n"
+"QPushButton{\n"
+"border-style:solid;\n"
+"\n"
+"background-color:#3d3d3d;\n"
+"color:#fff;\n"
+"border-radius:7px;\n"
+"}\n"
+"QPushButton:hover{\n"
+"color:#ccc;\n"
+"	background-color: qlineargradient(spread:pad, x1:0.517, y1:0, x2:0.517, y2:1, stop:0 rgba(45, 45, 45, 255), stop:0.505682 rgba(45, 45, 45, 255), stop:1 rgba(29, 29, 29, 255));\n"
+"	border-color:#2d89ef;\n"
+"border-width:2px;\n"
+"}\n"
+"\n"
+"QPushButton:pressed{\n"
+"background-color: qlineargradient(spread:pad, x"
+                        "1:0.517, y1:0, x2:0.517, y2:1, stop:0 rgba(29, 29, 29, 255), stop:0.505682 rgba(45, 45, 45, 255), stop:1 rgba(29, 29, 29, 255));\n"
+"}\n"
+"\n"
+"\n"
+"QTabWidget::tab{\n"
+"background-color:#3d3d3d;\n"
+"}\n"
+"\n"
+"QLineEdit{\n"
+"border-radius:0;\n"
+"}\n"
+"\n"
+"QProgressBar{\n"
+"border-radius:0;\n"
+"text-align:center;\n"
+"color:#fff;\n"
+"background-color:transparent;\n"
+"border: 2px solid #e3a21a;\n"
+"border-radius:7px;\n"
+"	font: 75 12pt \"Open Sans\";\n"
+"\n"
+"}\n"
+"\n"
+"QProgressBar::chunk{\n"
+"background-color:#2d89ef;\n"
+"width:20px;\n"
+"}"));
+        LoginWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(LoginWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        LoginWindow->setStatusBar(statusBar);
+
+        retranslateUi(LoginWindow);
+
+        QMetaObject::connectSlotsByName(LoginWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *LoginWindow)
+    {
+        LoginWindow->setWindowTitle(QApplication::translate("LoginWindow", "Login", 0));
+        groupBox->setTitle(QString());
+        UsernameLineEdit->setInputMask(QString());
+        UsernameLineEdit->setText(QString());
+        UsernameLineEdit->setPlaceholderText(QApplication::translate("LoginWindow", "Enter Username", 0));
+        PasswordLineEdit->setText(QString());
+        PasswordLineEdit->setPlaceholderText(QApplication::translate("LoginWindow", "Enter Password", 0));
+        LoginButton->setText(QApplication::translate("LoginWindow", "Login", 0));
+        SignUpButton->setText(QApplication::translate("LoginWindow", "Sign up", 0));
+        LogoLabel->setText(QApplication::translate("LoginWindow", "TextLabel", 0));
+        authError->setText(QApplication::translate("LoginWindow", "*Re-enter usename and password!", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class LoginWindow: public Ui_LoginWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_LOGINWINDOW_H
